4ee43eaf4b4070ba1ca9fcf3fd3a775ec5e6d286
IanEzeP <pereyra.ian00@gmail.com>
2024-06-27 15:24:02 -0300

    Unificación de rama local y remota

--- android/app/src/main/java/com/AFI2024/app/MainActivity.java
+++ android/app/src/main/java/com/AFI2024/app/MainActivity.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:android/app/src/main/java/pps/afi/parcial/MainActivity.java
-package pps.afi.parcial;
-========
 package com.AFI2024.app;
->>>>>>>> e29289119e1382e6660fe9e648086ba34a65f91a:android/app/src/main/java/com/AFI2024/app/MainActivity.java
 
 import com.getcapacitor.BridgeActivity;
 
--- android/app/src/main/java/pps/afi/parcial/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package pps.afi.parcial;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
